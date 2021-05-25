f90098ef60cb4d503d9f98e90236b9179bd9a8de
khemraj <khemrajkashyap010@gmail.com>
2021-05-25 15:55:03 +0530

    services

---
+++ app/src/main/java/com/example/serviceplaymusicbackground/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.serviceplaymusicbackground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //button objects
+    private Button buttonStart;
+    private Button buttonStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting buttons from xml
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+
+        //attaching onclicklistener to buttons
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonStart) {
+            //starting service
+            startService(new Intent(this, MyService.class));
+        } else if (view == buttonStop) {
+            //stopping service
+            stopService(new Intent(this, MyService.class));
+        }
+    }
+}
